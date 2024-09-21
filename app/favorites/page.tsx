535de700b84835f52548bb3a7c5459beef54bf6b
Marioo <marioyoussef936@gmail.com>
2024-09-21 20:58:17 +0300

    fix  features

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,18 +1,24 @@
 import { getFavoritedProducts } from "@/Backend/actions/FetchFavorites";
 import SectionTitle from "@/components/global/SectionTitle";
 import ProductsGrid from "@/components/products/ProductsGrid";
-
+import { currentUser } from "@clerk/nextjs/server";
 async function FavoritesPage() {
-    const favorites = await getFavoritedProducts();
-    console.log(favorites)
-    if (favorites?.length === 0)
-      return <SectionTitle text='You have no favorites yet.' />;
-    return (
-      <div>
-        <SectionTitle text='Favorites' />
-        <ProductsGrid products={favorites}/>
-      </div>
-    );
+  const user = await currentUser();
+  const email = user?.primaryEmailAddress?.emailAddress;
+  if (!email) {
+    console.error("User email not found");
+    return [];
   }
-  
-  export default FavoritesPage;
+  const favorites = await getFavoritedProducts();
+  console.log(favorites);
+  if (favorites?.length === 0)
+    return <SectionTitle text="You have no favorites yet." />;
+  return (
+    <div>
+      <SectionTitle text="Favorites" />
+      <ProductsGrid products={favorites} email={email} />
+    </div>
+  );
+}
+
+export default FavoritesPage;
